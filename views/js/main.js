2e4d46bca62a99532e5a343ef38ec871d73e13ef
Chris Archibald <carchi8py@gmail.com>
2015-07-01 12:21:37 -0700

    Optimized Event Listener

--- views/js/main.js
+++ views/js/main.js
@@ -526,16 +526,17 @@ function updatePositions() {
 // runs updatePositions on scroll
 window.addEventListener('scroll', updatePositions);
 
+var pizzaElement = document.createElement("img")
+pizzaElement.className = "mover"
+pizzaElement.src = "images/pizza.png";
+pizzaElement.style.height = "100px";
+pizzaElement.style.width = "73.333px";
 // Generates the sliding pizzas when the page loads.
 document.addEventListener('DOMContentLoaded', function() {
   var cols = 8;
   var s = 256;
   for (var i = 0; i < 200; i++) {
-    var elem = document.createElement('img');
-    elem.className = 'mover';
-    elem.src = "images/pizza.png";
-    elem.style.height = "100px";
-    elem.style.width = "73.333px";
+    var elem = pizzaElement.cloneNode(true);
     elem.basicLeft = (i % cols) * s;
     elem.style.top = (Math.floor(i / cols) * s) + 'px';
     document.querySelector("#movingPizzas1").appendChild(elem);
